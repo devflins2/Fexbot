a13e6ce0fe919e125dc91b2bd852ce5b3b8cf4a5
Gaurav <gauravkushwaha990111@gmail.com>
2026-04-03 14:12:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN || '8402337975:AAFcTQHy98FMAqP2NgxrOsZi8Ea83JHIjIc',
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://genshingaurav11_db_user:yvzHB0fJxcOKiTvb@fexbot.htstawp.mongodb.net/?appName=fexbot',
-    MAIN_ADMIN_ID: parseInt(process.env.MAIN_ADMIN_ID) || 6155928882, // Your Telegram ID here
-    ADMIN_PASS: process.env.ADMIN_PASS || 'sujoylnd'
+    BOT_TOKEN: process.env.BOT_TOKEN || 
+    MONGO_URI: process.env.MONGO_URI || 
+    MAIN_ADMIN_ID: parseInt(process.env.MAIN_ADMIN_ID) ||  // Your Telegram ID here
+    ADMIN_PASS: process.env.ADMIN_PASS || 
 };
